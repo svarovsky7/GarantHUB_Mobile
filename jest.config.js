0ea20cc7514eb5bf6b139aad7808c0866c16a5d9
svarovsky7 <46369931+svarovsky7@users.noreply.github.com>
2025-07-12 20:42:08 +0300

    Revert "Добавлены экраны входа и регистрации"

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,3 @@
 module.exports = {
   preset: 'react-native',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation)/)',
-  ],
 };
--- shared/ui/Button.tsx
+++
@@ -1,5 +0,0 @@
-import { Button as PaperButton, ButtonProps } from 'react-native-paper';
-
-export function Button(props: ButtonProps) {
-  return <PaperButton mode="contained" {...props} />;
-}
